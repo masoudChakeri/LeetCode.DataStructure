fd2e07eb54e0dcb3d7f2c3fea96f7ce3d1008daa
masoudChakeri <masoud.chakeri@gmail.com>
2022-09-10 21:03:16 +0430

    contains duplicate submitted.

---
+++ LeetCode.DataStructure.ContainsDuplicate/Program.cs
@@ -0,0 +1,25 @@
+﻿bool ContainsDuplicate(int[] nums)
+{
+    // for (int i = 0; i < nums.Length; i++)
+    // {
+    //     for (int j = i + 1; j < nums.Length; j++)
+    //     {
+    //         if (nums[i] == nums[j])
+    //         {
+    //             return true;
+    //         }
+    //     }
+    // }
+    //
+    // return false;
+
+    if (nums.Length != nums.Distinct().Count())
+    {
+        return true;
+    }
+
+    return false;
+}
+
+int[] nums = { 1, 2, 3, 4 };
+Console.WriteLine(ContainsDuplicate(nums));
---
+++ LeetCode.DataStructure.MaximumSubarray/Program.cs
@@ -0,0 +1,4 @@
+﻿int MaxSubArray(int[] nums)
+{
+
+}
